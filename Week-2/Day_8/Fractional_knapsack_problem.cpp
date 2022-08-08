2eb8e8f4c61b008f4d5c99ccfd88438321cab1b2
Chirag Varshney <55586574+ChiragV21@users.noreply.github.com>
2022-08-08 23:49:50 +0530

    Add files via upload

---
+++ Week-2/Day_8/Fractional_knapsack_problem.cpp
@@ -0,0 +1,45 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	int ans=1; 
+    for(int i=0;i<=n-1;i++)
+    {
+        int count=1; 
+        for(int j=i+1;j<=n-1;j++)
+        {
+            if((arr[i]>=arr[j] && arr[i]<=dep[j]) ||
+           (arr[j]>=arr[i] && arr[j]<=dep[i]))
+           {
+               count++;
+           }
+        }
+        ans=max(ans,count); 
+    }
+    return ans;
+    }
+};
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
